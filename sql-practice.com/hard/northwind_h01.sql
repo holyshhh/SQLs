97154fd2436cdd12ccbd955e8988c7cd609adde1
Sihwa Lee <50615678+129hwa@users.noreply.github.com>
2026-05-07 00:15:14 +0900

    Rename

--- sql-practice.com/hard/northwind_h01.sql
+++ sql-practice.com/hard/northwind_h01.sql
@@ -1,7 +1,9 @@
-# Show the employee's first_name and last_name, a "num_orders" column with a count of the orders taken,
-# and a column called "Shipped" that displays "On Time" if the order shipped_date is less or equal to the required_date,
-# "Late" if the order shipped late, "Not Shipped" if shipped_date is null.
-# Order by employee last_name, then by first_name, and then descending by number of orders.
+/*
+Show the employee's first_name and last_name, a "num_orders" column with a count of the orders taken,
+and a column called "Shipped" that displays "On Time" if the order shipped_date is less or equal to the required_date,
+"Late" if the order shipped late, "Not Shipped" if shipped_date is null.
+Order by employee last_name, then by first_name, and then descending by number of orders.
+*/
 
 SELECT
     first_name,
